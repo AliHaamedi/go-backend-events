9044b578c4bed598f6cf64a1107633941e14601f
AliHaamedi <alihaamedin@gmail.com>
2024-01-10 11:31:48 +0330

    update db

--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,13 @@ import (
 )
 
 func RegisterRoute(server *gin.Engine) {
+	//events
 	server.GET("/events", controllers.GetEvents)
 	server.POST("/events", controllers.CreateEvent)
 	server.GET("/events/:id", controllers.GetEvent)
 	server.PUT("/events/:id", controllers.UpdateEvent)
 	server.DELETE("/events/:id", controllers.DeleteEvent)
+
+	//users
+	server.POST("/signup")
 }
